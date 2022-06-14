5e2f9712da01b57bdb82bbf4e79683fc65563f72
Anton Volskyi <antonvolskyi1234@gmail.com>
2022-06-14 17:48:12 +0300

    Changet main

--- src/StartWindow.java
+++ src/StartWindow.java
@@ -1,6 +1,6 @@
 import display.WelcomeWindow;
 
-public class Main {
+public class StartWindow {
     public static void main(String[] args) {
         WelcomeWindow welcomeWindow = new WelcomeWindow();
         welcomeWindow.createWelcomeWindow();
